917b0476f13f16d0c265bcc2616b28f66944f9e1
akshsekhr2702 <akshyansunayak360985@gmail.com>
2023-08-17 13:57:15 +0530

    Added pomodoro v1

---
+++ other_applications/pomodoro-akshay/script.js
@@ -0,0 +1,64 @@
+let seconds = 60;
+minutes = 25;
+let interval1;
+console.log("working")
+
+
+// Local storage
+let name = localStorage.getItem('name')
+console.log(name)
+document.querySelector('.username').innerHTML = `Hi ${name}`
+// localStorage.
+
+
+// document.getElementById('secContainer1').innerHTML = 60;
+// document.getElementById('minContainer1').innerHTML = 2;
+
+function startPomo(){
+     interval1 = setInterval(min, 10)
+    document.querySelector('.start').disabled = true;
+
+}
+
+function min(){
+    seconds = seconds -1 
+    document.querySelector('#secContainer1').innerHTML = seconds;
+    if(seconds === 0){
+        seconds = 60;
+        minutes = minutes - 1
+        document.querySelector('#minContainer1').innerHTML = minutes;
+        if(minutes === -1){
+            console.log('min is 0')
+            console.log(seconds)
+            clearInterval(interval1)
+            document.querySelector('#minContainer1').innerHTML = 0;
+
+
+        }
+    }
+
+}
+function resetPomo(){
+  interval1 = clearInterval()
+}
+
+
+// function min(){
+//     // seconds = document.getElementById('secContainer1').innerHTML;
+//     seconds = seconds - 1;
+//     document.getElementById('secContainer1').innerHTML = seconds;
+//     if (seconds === 0){
+      
+//         seconds = 10;
+//         minutes = minutes - 1;
+//         document.getElementById('minContainer1').innerHTML = minutes;
+//         document.getElementById('secContainer1').innerHTML = seconds;
+       
+
+//     }
+//     if(minutes === 0 && seconds === 0){
+//         document.getElementById('minContainer1').innerHTML = 0;
+//         document.getElementById('secContainer1').innerHTML = 0;
+//     }
+   
+// }
