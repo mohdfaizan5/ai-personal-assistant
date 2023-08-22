00c2dc5881f3ac9433757532ac646f9e06d89e20
Mohammed Tahir <mohammedtahir65080@gmail.com>
2023-08-22 13:00:27 +0530

    Tahir to-do without local storage

--- other_applications/tahir-todo/script.js
+++ other_applications/tahir-todo/script.js
@@ -1,3 +1,4 @@
+/*
 function addTask(){
   
   let userInput = document.querySelector('#input-box')
@@ -9,9 +10,33 @@ function addTask(){
 
   // change task1
   finalDisplay.appendChild(userValue)
-
-
-
-
-
 }
+*/
+
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask() {
+  if (inputBox.value === ''){
+    alert('List any task first...')
+  }
+  else{
+    let li = document.createElement("li");    // Create element
+    li.innerHTML = inputBox.value;    // store value inside
+    listContainer.appendChild(li);    // to Display
+
+    let span = document.createElement("span")
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+}
+
+listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+  }
+},false);
